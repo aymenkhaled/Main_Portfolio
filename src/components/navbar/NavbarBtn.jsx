1720f0e43ed98146dbd78ca9888deeb256724f5d
aymenkhaled <89142557+aymenkhaled@users.noreply.github.com>
2025-08-06 02:42:52 +0200

    Update NavbarBtn.jsx

--- src/components/navbar/NavbarBtn.jsx
+++ src/components/navbar/NavbarBtn.jsx
@@ -3,7 +3,7 @@ import { LuArrowDownRight } from "react-icons/lu";
 const NavbarBtn = () => {
   return (
     <a
-    href="/Khaled_Aymen_CV.pdf"
+    href="/Aymen_Khaled_CV.pdf"
     target="_blank"
     rel="noopener noreferrer"
     className="px-4 py-2 rounded-full text-xl font-bold font-body text-white border-cyan border flex items-center gap-1 bg-gradient-to-r from-darkCyan to-orange transition-all duration-500 hover:scale-110 hover:border-orange cursor-pointer hover:shadow-cyanShadow"
